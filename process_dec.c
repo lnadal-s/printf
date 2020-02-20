e8c2edaeca4468344e5c1161574887743b31e1c1
Lorenzo NADAL SANTA <lnadal-s@student.42.fr>
2020-02-20 09:37:20 +0100

    push

--- process_dec.c
+++ process_dec.c
@@ -6,11 +6,18 @@
 /*   By: lnadal-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 17:45:45 by lnadal-s          #+#    #+#             */
-/*   Updated: 2020/02/19 18:20:25 by lnadal-s         ###   ########.fr       */
+/*   Updated: 2020/02/20 09:36:53 by lnadal-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+
+int get_prec(t_arg *arg, char *dst, int size)
+{
+	char *prec;
+
+	if (!(w = alloc_char('o', size)))
+}
 
 int get_res_dec(char *nbr, t_arg *arg)
 {
@@ -19,15 +26,20 @@ int get_res_dec(char *nbr, t_arg *arg)
 	char *w;
 	char *dst;
 	int size;
-
-	if (arg->alig == 1)
-		sp = ' ';
-	else if (arg->zero == 1)
-		sp = '0';
+	
 	size = (int)ft_strlen(nbr);
 	if (arg->is_prec == 1)
 		size = arg->prec > size ? arg->prec : size;
-	if (!(dst = (char *)
+	if (!(dst = (char *)malloc(sizeof(char) * (size + 1))))
+		return (0);
+	dst = ft_strlcpy(dst, nbr, size + 1);
+	get_prec(arg, dst, size);
+
+	if (arg->alig == 1 || arg->is_prec == 1)
+		sp = ' ';
+	else if (arg->is_prec == 0 && arg->zero == 1)
+		sp = '0';
+	if (arg-
 }
 
 int process_dec(t_print *tp, t_arg *arg)
